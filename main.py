8cc540fa69a472248465f234925921d65cf9c460
Shahmeer0311 <22alis@students.watfordboys.org>
2024-12-28 14:30:43 +0000

    Weather Conditions

---
+++ main.py
@@ -0,0 +1,20 @@
+def weatherCondition ():
+    print("The weather is pleasant in ", spring )
+    print("The weather is same in ", Autumn )
+spring = "Autumn"
+Autumn = "spring"
+weatherCondition()
+
+def  Test (a,b):
+    print("My name is ", a )
+    print("My friends name is ", b )    
+Test("Shahmeer", "Yusuf")
+Test("Shahmeer1", "Yusuf1")
+
+
+def  Test2 (c,d):
+    print("My Maths marks are ", c )
+    print("My science marks are  ", d )    
+maths = int(input("Enter your marks in Maths"))
+Science = int(input("Enter your marks in Science"))
+Test2(maths,Science)
